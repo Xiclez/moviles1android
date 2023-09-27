e7ec88d13d5a1fbaa03baa264b4cb6ee868d289d
Xiklez <6614@ulsachihuahua.edu.mx>
2023-09-27 14:37:01 -0600

    Onboarding screen

---
+++ Onboarding/app/src/main/java/com/example/onboarding/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.onboarding.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val Blue200 = Color(0xFF4169E1)
+val bgBlue = Color(0xFF9BC9F9)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
